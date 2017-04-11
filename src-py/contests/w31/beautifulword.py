6e48673741654917c080d26d8bb67fd8c6d8c269
Anuvrat Singh <anuvrat@amazon.com>
2017-04-10 20:10:08 -0700

    Solved the first problem of Week of Code 31

---
+++ src-py/contests/w31/beautifulword.py
@@ -0,0 +1,26 @@
+from collections import namedtuple
+
+vowels = "aeiouy"
+consonants = "bcdfghjklmnpqrstvwyxz"
+
+Input = namedtuple('Input', ['word', 'expected'])
+
+
+class BeautifulWord(object):
+    def is_beautiful(self, word):
+        last_char = word[0]
+
+        for idx in range(1, len(word)):
+            char = word[idx]
+
+            if char == last_char or (char in vowels and last_char in vowels): return False
+
+            last_char = char
+
+        return True
+
+
+if __name__ == '__main__':
+    w = input().strip()
+
+    print('Yes' if BeautifulWord().is_beautiful(w) else 'No')
---
+++ src-py/contests/w31/beautifulwordtest.py
@@ -0,0 +1,12 @@
+import unittest
+from contests.w31.beautifulword import BeautifulWord
+
+
+class TestStringMethods(unittest.TestCase):
+    def test_1(self):
+        w = 'abacaba'
+        self.assertTrue(BeautifulWord().is_beautiful(w))
+
+    def test_2(self):
+        w = 'badd'
+        self.assertFalse(BeautifulWord().is_beautiful(w))
